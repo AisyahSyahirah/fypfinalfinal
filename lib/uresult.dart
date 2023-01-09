713f0366c9d9f5dbea0e8818c4996ce37ca2fa13
AisyahSyahirah <aisyah.syahirah@live.iium.edu.my>
2023-01-10 02:25:13 +0800

    ai

--- lib/uresult.dart
+++ lib/uresult.dart
@@ -27,23 +27,6 @@ class _UresultState extends State<Uresult> {
                 TextStyle(fontSize: 20, fontWeight: FontWeight.bold, height: 3),
             textAlign: TextAlign.center,
           ),
-<<<<<<< HEAD
-          // Container(
-          //   padding: const EdgeInsets.all(10),
-          //   child: const Text(
-          //     "Event:",
-          //     style: TextStyle(fontWeight: FontWeight.bold),
-          //   ), //kat sini nak panggil balik event yg dia choose kat dropdown tadi
-          // ),
-          // Container(
-          //   padding: const EdgeInsets.all(10),
-          //   child: const Text(
-          //     "Tournament:",
-          //     style: TextStyle(fontWeight: FontWeight.bold),
-          //   ), //kat sini nak panggil balik tournament yg dia choose kat dropdown tadi
-          // ),
-=======
->>>>>>> 6041dba4e71606b6e299d76a167a15620a4fd3bd
           DataTable(
             columns: const [
               DataColumn(
